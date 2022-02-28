d9f39b229c36d5e7ce9bd20898183bce20493380
sanketsaagar <sanketsaagar1234@gmail.com>
2022-02-28 19:29:41 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -4,3 +4,11 @@ print(mystring)
 mystring1 = "My second name is Saagar"
 print(mystring1)
 print(type(mystring1))
+
+help_message = '''
+My name
+is Sanket
+and I am not 
+good
+'''
+print(help_message)

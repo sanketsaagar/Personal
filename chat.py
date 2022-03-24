ec90ea027124ffec885ecf2ea14ca22e271e8172
sanketsaagar <sanketsaagar1234@gmail.com>
2022-03-24 11:47:21 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,16 +3,15 @@ Dict = {'Hey there, How are you?':'Fine thanks',
         'Where are you from?':'Jharkhand',
         'How was your day?':'Awesome!'}
 
-I = input("Hello! Please shoot question: ")
+I = input("Hello! Please shoot question : ")
 
 while(True):    
     if(I in Dict):
         print(Dict[I])
-        I= input()
+        I= input("Next Question please : ")
     elif(I=='See you'):
         print("Sure, See you later")
         break
     else:
         print('Please ask valid questions')
         break
-    

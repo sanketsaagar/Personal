7f99edde479a20211d92db6c3fde0b1320a15d3b
sanketsaagar <sanketsaagar1234@gmail.com>
2022-05-05 18:20:08 +0530

    Create pa.py

---
+++ pa.py
@@ -0,0 +1,18 @@
+def add():
+    pass
+
+def sub():
+    pass
+
+def mul():
+    pass
+if 5==5:
+    pass
+else:
+    pass
+
+students=["Sanket","Saagar","Karan"]
+for mystudents in students:
+    pass
+
+print("This is xyz statement!!!")

07463f164f41cd98e98e6b6e267102f5a930b9d1
sanketsaagar <sanketsaagar1234@gmail.com>
2022-05-24 19:35:25 +0530

    Update sub.py

---
+++ sub.py
@@ -0,0 +1,10 @@
+student1 = "The student name {} is learning Blockchain"
+
+student2 = student1.format("Sanket")
+student3 = student1.format("Mohit")
+
+print(student2)
+print(student3)
+
+x= 56.236
+print("%.2f"%x)

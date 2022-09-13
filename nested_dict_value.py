6e72d0d981ddcff596e8f2d6d41852d3460ba19b
sanketsaagar <sanketsaagar1234@gmail.com>
2022-09-13 12:02:37 +0530

    Important Programs on Python

--- nested_dict_value.py
+++ nested_dict_value.py
@@ -1,1 +1,8 @@
 #Change value of a key in a nested dictionary
+
+student1 = {'Ram':{'Height': '5','Colour':'White', 'Place':'Ranchi'},'Mohan':{'Height':'6','Colour':'Brown','Place':'Ramgarh'}}
+print('Initial Dict is:-', student1)
+
+student1['Mohan']['Colour'] = 'Dark'
+
+print('The new Dic is :-', student1)
--- value_exists.py
+++ value_exists.py
@@ -1,6 +1,7 @@
 #Check if a value exists in a Dictionary
 
 student1 = {'Maths':34, 'Science':65, 'English':45, 'Hindi':76 }
+print('The Original Dictionary is:- ', str(student1))
 
 if 'Social' in student1:
     print(' Exists')

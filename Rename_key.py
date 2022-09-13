379327d61137f0c754cb9f13e1ade6d9350e9626
sanketsaagar <sanketsaagar1234@gmail.com>
2022-09-13 11:42:50 +0530

    Added programs

---
+++ Rename_key.py
@@ -0,0 +1,10 @@
+#Rename a key of a Dictionary
+
+student1 = {'Maths':34, 'Science':65, 'English':45, 'Hindi':76 }
+
+print('The Original Dictionary is:- ', student1) #Printing the original Dictionary
+
+student1['Social Science'] = student1['Science']
+del student1['Science']
+
+print('After Renaming the Dictionary :-', student1)
---
+++ value_exists.py
@@ -0,0 +1,9 @@
+#Check if a value exists in a Dictionary
+
+student1 = {'Maths':34, 'Science':65, 'English':45, 'Hindi':76 }
+
+if 'Social' in student1:
+    print(' Exists')
+
+else:
+    print(' Dont Exists')

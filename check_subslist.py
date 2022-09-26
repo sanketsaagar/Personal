7933154fe2654502b8561640dc82fb2363ac7d9b
sanketsaagar <sanketsaagar1234@gmail.com>
2022-09-26 16:35:44 +0530

    Some correction

--- check_subslist.py
+++ check_subslist.py
@@ -1,15 +1,16 @@
 #Q3. Write a python Program to check if a subslist is present in a list or not
 
 orgList = [2,4,5,8,7,1,6,9]
+subList = [11,3,12]
 
 print( "The Original list is:- " + str(orgList))
+print("The Sublist is:- " +str(subList))
 
-subList = [5,8,6]
 
-res = any(orgList[idx : idx + len(subList)] == subList
-        for idx in range(len(orgList) - len(subList) + 1))
-         
-print("Is sublist present in list ? : " + str(res))
+if(all(x in orgList for x in subList)):
+        print("Yes SubList is part of OrgList")
+else:
+        print("No, SubList is not part of OrgList")
 
 
 

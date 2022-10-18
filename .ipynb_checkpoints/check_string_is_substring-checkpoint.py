0356c1255e85aa2d2b73e01e07d30f7c77033831
sanketsaagar <sanketsaagar1234@gmail.com>
2022-10-18 10:16:12 +0530

    Metplotlip programs

---
+++ .ipynb_checkpoints/check_string_is_substring-checkpoint.py
@@ -0,0 +1,8 @@
+
+str1 = "Hi I am Sanket"
+str2 = "my"
+
+if str2 in str1:
+    print("Yes str2 is substring of str1")
+else:
+    print("No, str2 is not substring of str1")
---
+++ nump.py
@@ -0,0 +1,4 @@
+
+import numpy as np;
+arr = np.arry([1,2,3,4,5])
+print (arr)

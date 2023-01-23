11e91e112429b223f6649001c537a4f4eea74496
sanketsaagar <sanketsaagar1234@gmail.com>
2023-01-24 00:23:18 +0530

    Update cov.py

--- cov.py
+++ cov.py
@@ -2,11 +2,11 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-A = [45, 37, 42, 35, 39]
-B = [38, 31, 26, 28, 33]
-C = [10, 15, 17, 21, 12]
+Maths = [45, 37, 42, 35, 39]
+English = [38, 31, 26, 28, 33]
+Science = [10, 15, 17, 21, 12]
 
-data = np.array([A, B, C])
+data = np.array([Maths, English, Science])
 
 cov_matrix = np.cov(data, bias=True)
 print(cov_matrix)
